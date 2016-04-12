1fb902ceb6cf1df6f114e3c5c6c86f9ee5547965
Davi Marcos <davi.blain@gmail.com>
2016-04-11 21:28:09 -0300

    Configurando Teste Automatizado

---
+++ spec/support/capybara.rb
@@ -0,0 +1,10 @@
+#capybara.rb
+
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+	Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 5000)
+end
+	
+Capybara.javascript_driver = :poltergeist
---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,5 @@
+#factory_girl.rb
+
+RSpec.configure do |config|
+	config.include FactoryGirl::Syntax::Methods
+end
